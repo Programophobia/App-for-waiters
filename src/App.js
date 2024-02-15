b149441ea21c713f4ebbb109f6fea80c77db0ae1
Anna Powaska <annapowaska@op.pl>
2024-02-15 08:31:15 +0100

    App structure established

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Container } from 'react-bootstrap';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    
-    <Container>
-   <section>
-        <h2 className="mb-4 text-center">Edit post</h2>
-</section>
-    </Container>
+    <main>
+     <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Main/>} />
+          <Route path="/table/:tableId" element={<Table />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Container>
+</main>
   )
 }
-
+<h2 className="mb-4 text-left">WAITER APP</h2>
 export default App;
